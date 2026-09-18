a4287f4f69a86b5b8f92e35a3a50cdff4fd359a7
hieu8206 <hahieuminecraft@gmail.com>
2026-09-18 22:58:54 +0700

    Add files via upload

---
+++ Hinh_tron.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Input
+{
+    class Program
+    {
+        static void Main()
+        {
+        Console.Write("Nhap ban kinh hinh tron: ");
+            double r = double.Parse(Console.ReadLine());
+            double S = Math.PI * r * r;
+            Console.WriteLine($"Dien tich hinh tron la: {S}");
+            double C = 2 * Math.PI * r;
+            Console.WriteLine($"Chu vi hinh tron la: {C}");
+        }
+    }
+}
